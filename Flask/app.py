9e95a77d464a561f773ac64a1def71eb57d4641d
Blaha <cblaha1@dbdev.cs.kent.edu>
2024-04-23 16:53:57 -0400

    Starting development of central webpage

--- Flask/app.py
+++ Flask/app.py
@@ -1,11 +1,13 @@
-from flask import Flask, request
+from flask import Flask, request, render_template,send_file
 from markupsafe import escape
 
 app = Flask(__name__)
 
+
+#index file
 @app.route('/')
-def hello():
-    name = request.args.get("name", "World")
-    return f'Hello, {escape(name)}!'
+def index():
+    return render_template('./template/index.html')
+
 if __name__ == "__main__":
     app.run(host='localhost', port=5005)
