97f17815a5d02139fc873097bc2db33b3f5b8b82
Blaha <cblaha1@dbdev.cs.kent.edu>
2024-04-23 16:58:05 -0400

    made minor change

--- Flask/app.py
+++ Flask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template,send_file
+from flask import Flask, request, render_template ,send_file
 from markupsafe import escape
 
 app = Flask(__name__)
@@ -7,7 +7,7 @@ app = Flask(__name__)
 #index file
 @app.route('/')
 def index():
-    return render_template('./template/index.html')
+    return render_template('./index.html')
 
 if __name__ == "__main__":
     app.run(host='localhost', port=5005)
